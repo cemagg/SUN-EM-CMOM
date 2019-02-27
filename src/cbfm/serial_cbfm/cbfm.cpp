c621619c81f2102fd72e40bde490e77edcd5f9b9
Tameez <etameez@gmail.com>
2019-02-27 12:18:52 +0200

    Added an outline for how cbfm will be done
    Former-commit-id: 291e4fa6bccd3da62433eb055ae3c15cdb027f12

--- src/cbfm/serial_cbfm/cbfm.cpp
+++ src/cbfm/serial_cbfm/cbfm.cpp
@@ -6,5 +6,19 @@ void performCBFM(std::map<std::string, std::string> &const_map,
                    std::vector<Node<double>> &nodes,
                    std::complex<double> *ilhs)
 {
-	
+	// Create necessary Zmn Matrices
+
+	// Create necessary Vm vectors
+
+	// Calculate Primary CBF's
+
+	// Calculate Secondary CBF's
+
+	// Calculate reduced Zcbfm
+
+	// Calculate reduced Vcbfm
+
+	// Calculate reduced Icbfm
+
+	// Solve Irwg
 }
