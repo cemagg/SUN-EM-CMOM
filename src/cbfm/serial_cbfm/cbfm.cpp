0310168ee3b288b2e710f9960f36653d58c51ba7
Tameez <etameez@gmail.com>
2019-02-27 12:10:23 +0200

    Added initial CBFM main serial call function
    Former-commit-id: b1caf8384e588b87d21acd75adb85fb431e2d78b

---
+++ src/cbfm/serial_cbfm/cbfm.cpp
@@ -0,0 +1,10 @@
+#include "cbfm.h"
+
+void performCBFM(std::map<std::string, std::string> &const_map,
+                   std::vector<Triangle> &triangles,
+                   std::vector<Edge> &edges,
+                   std::vector<Node<double>> &nodes,
+                   std::complex<double> *ilhs)
+{
+	
+}
