c16ec761df9cfbd1f2db780b7604ee5ca3353255
Tameez <etameez@gmail.com>
2019-02-27 15:08:43 +0200

    Rough outline of cbfm

--- src/cbfm/serial_cbfm/cbfm.cpp
+++ src/cbfm/serial_cbfm/cbfm.cpp
@@ -1,24 +1,36 @@
 #include "cbfm.h"
 
 void performCBFM(std::map<std::string, std::string> &const_map,
+				 std::map<int, std::vector<int>> &label_map,
                    std::vector<Triangle> &triangles,
                    std::vector<Edge> &edges,
                    std::vector<Node<double>> &nodes,
                    std::complex<double> *ilhs)
 {
-	// Create necessary Zmn Matrices
+	// Define some constants
+	int num_domains = label_map.size(); // number of domains
 
-	// Create necessary Vm vectors
+	//-- Create necessary Zmn Matrices --//
+	
+	// Create struct to store all z matrices and their inverses
+	CBFMZMatrices v_mom_z; // Struct storing the vanilla mom Zmn matrices
 
-	// Calculate Primary CBF's
+	// Lets resize all the struct elements to fit the problem
+	resizeCBFMZMatricesForEqualDomains(v_mom_z, num_domains, label_map[0][3]);	
 
-	// Calculate Secondary CBF's
+	//-- Create necessary Vm vectors --//
+	CBFMVectors v_mom_v;;
+		
 
-	// Calculate reduced Zcbfm
+	//-- Calculate Primary CBF's --//
 
-	// Calculate reduced Vcbfm
+	//-- Calculate Secondary CBF's --//
 
-	// Calculate reduced Icbfm
+	//-- Calculate reduced Zcbfm --//
 
-	// Solve Irwg
+	//-- Calculate reduced Vcbfm --//
+
+	//-- Calculate reduced Icbfm --//
+
+	//-- Solve Irwg --//
 }
