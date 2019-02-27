c30d663170726805ca5010e172c064ea3d901926
Tameez <etameez@gmail.com>
2019-02-27 15:09:04 +0200

    Creating and resizing initial data structures

---
+++ src/cbfm/cbfm_helpers/cbfm_helpers.cpp
@@ -0,0 +1,44 @@
+#include "cbfm_helpers.h"
+
+
+
+void resizeCBFMZMatricesForEqualDomains(CBFMZMatrices &z_matrices, int num_domains, int num_edges_per_domain)
+{
+	// Allocate memory for z_self, z_self_inv, and z_self_piv
+	z_matrices.z_self = new std::complex<double>[num_edges_per_domain * num_edges_per_domain]();
+	z_matrices.z_self_inv = new std::complex<double>[num_edges_per_domain * num_edges_per_domain]();
+	z_matrices.z_self_piv = new std::complex<double>[num_edges_per_domain]();
+
+	// Allocate for z_couple, z_couple_inv and z_couple_piv
+
+	// First resize the main vectors
+	z_matrices.z_couple.resize(num_domains);
+	z_matrices.z_couple_inv.resize(num_domains);
+	z_matrices.z_couple_piv.resize(num_domains);
+
+	// now resize the secondary vectors and allocate to the pointers
+	for(int i = 0; i < num_domains; i++)
+	{
+		z_matrices.z_couple[i].resize(num_domains);
+		z_matrices.z_couple_inv[i].resize(num_domains);
+		z_matrices.z_couple_piv[i].resize(num_domains);
+		
+		for(int j = 0; j < num_domains; j++)
+		{
+			z_matrices.z_couple[i][j] = new std::complex<double>[num_edges_per_domain * num_edges_per_domain]();
+			z_matrices.z_couple_inv[i][j] = new std::complex<double>[num_edges_per_domain * num_edges_per_domain]();
+			z_matrices.z_couple_piv[i][j] = new std::complex<double>[num_edges_per_domain]();
+		}
+	}	
+}
+
+
+void resizeCBFMVectorsForEqualDomains(CBFMVectors &v_vectors, int num_domains, int num_edges_per_domain, bool resize_j_cbfm)
+{
+	if(!resize_j_cbfm)
+	{
+		// Resize the v_self, j_prim and j_sec(main) vectors 
+		v_vectors.v_self.resize(num_domains);
+		v_vectors.j_prim.resize(num_domains);
+	}
+}
