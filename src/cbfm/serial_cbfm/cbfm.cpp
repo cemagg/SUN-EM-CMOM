884ed13b9c7c7c4520eb18087dfe0529fa7da7b2
Tameez <etameez@gmail.com>
2019-02-28 20:35:29 +0200

    Added calculation of v_self + modified zmn to use new label struct

--- src/cbfm/serial_cbfm/cbfm.cpp
+++ src/cbfm/serial_cbfm/cbfm.cpp
@@ -1,7 +1,7 @@
 #include "cbfm.h"
 
 void performCBFM(std::map<std::string, std::string> &const_map,
-				 std::map<int, std::vector<int>> &label_map,
+				 std::map<int, Label> &label_map,
                    std::vector<Triangle> &triangles,
                    std::vector<Edge> &edges,
                    std::vector<Node<double>> &nodes,
@@ -16,7 +16,7 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 	CBFMZMatrices v_mom_z; // Struct storing the vanilla mom Zmn matrices
 
 	// Lets resize all the struct elements to fit the problem
-	resizeCBFMZMatricesForEqualDomains(v_mom_z, num_domains, label_map[0][3]);	
+	resizeCBFMZMatricesForEqualDomains(v_mom_z, num_domains, label_map[0].edge_indices.size());	
 
 	// Lets fill the Z matrices
 
@@ -26,21 +26,20 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 				  triangles,
 				  nodes,
                   const_map,
-                  label_map[0][0],
-                  label_map[0][1]+1,
-                  label_map[0][0],
-                  label_map[0][1]+1);
+                  label_map[0],
+                  label_map[0],
+                  false);
 
 	std::cout << "---------------------------------------Z_SELF------------------------------------------" << std::endl;
-	for(int i = 0; i < 4; i++)
+	for(int i = 0; i < label_map[0].edge_indices.size(); i++)
 	{
-		for(int j = 0; j < 4; j++)
+		for(int j = 0; j < label_map[0].edge_indices.size(); j++)
 		{
 			std::cout << v_mom_z.z_self[j + i * 4];
 		}
 		std::cout << std::endl;
 	}
-	std::cout << "---------------------------------------------------------------------------------------" << std::endl;
+	std::cout << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
 
 	// Lets now fill in v_mom_z.z_couple
 	int index;
@@ -58,12 +57,10 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 				  			  triangles,
 				  			  nodes,
                   		 	  const_map,
-                  		  	  label_map[i][0],
-                  		  	  label_map[i][2],
-                  		  	  label_map[j][0],
-                  		  	  label_map[j][2]);            
+                  		  	  label_map[i],
+                  		  	  label_map[j],
+                  		  	  true);            
                 index++; 
-                // std::cout << i  << " <+> " << j << std::endl;
 			}
 		}	
 	}
@@ -71,23 +68,39 @@ void performCBFM(std::map<std::string, std::string> &const_map,
     for(int m = 0; m < num_domains; m++){
     for(int n = 0; n < (num_domains - 1); n++){	
     std::cout << "--------------------------------------Z_COUP"<<m<<n<<"-----------------------------------------" << std::endl;
-    for(int i = 0; i < 4; i++)
+    for(int i = 0; i < label_map[0].edge_indices.size(); i++)
     {
-        for(int j = 0; j < 4; j++)
+        for(int j = 0; j < label_map[0].edge_indices.size(); j++)
         {
             std::cout << v_mom_z.z_couple[m][n][j + i * 4];
         }
         std::cout << std::endl;
     }
-    std::cout << "---------------------------------------------------------------------------------------" << std::endl;
+    std::cout << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
     }}
 	//-- Create necessary Vm vectors --//
 	CBFMVectors v_mom_v;
 
 	// Lets resize some of the cbfm vectors
-	//resizeCBFMVectorsForEqualDomains(v_mom_v, num_domains, label_map[0][3], false);
+	resizeCBFMVectorsForEqualDomains(v_mom_v, num_domains, label_map[0].edge_indices.size(), false);
 
-
+	// Lets now fill v_self vectors
+	for(int i = 0; i < num_domains; i++)
+	{
+		serialFillVrhs(const_map,
+					   triangles,
+					   edges,
+					   v_mom_v.v_self[i],
+					   label_map[i]);
+	}
+	for(int m = 0; m < num_domains; m++){
+    std::cout << "--------------------------------------V_SELF"<<m<<"------------------------------------------" << std::endl;
+    for(int i = 0; i < label_map[0].edge_indices.size(); i++)
+    {
+    	std::cout << v_mom_v.v_self[m][i] << std::endl;
+    }
+    std::cout << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
+    }
 	//-- Calculate Primary CBF's --//
 
 	//-- Calculate Secondary CBF's --//
