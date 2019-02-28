7e7a18c5e8f6102c207ff2235cd839780c53809e
Tameez <etameez@gmail.com>
2019-02-28 21:11:46 +0200

    Added now calculation of primary CBF's
    Former-commit-id: 68019740f92e73753ec4a40941a5b71a6b1083ed

--- src/cbfm/serial_cbfm/cbfm.h
+++ src/cbfm/serial_cbfm/cbfm.h
@@ -6,6 +6,8 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <cstring>
+#include <fstream>
 
 #include "../../data_structures/node.h"
 #include "../../data_structures/edge.h"
@@ -14,6 +16,12 @@
 #include "../../mom/serial_mom/fill_zmn.h"
 #include "../../mom/serial_mom/fill_vrhs.h"
 #include "../cbfm_helpers/cbfm_helpers.h"
+
+extern "C"
+{
+    void zgetrf_(int*, int*, std::complex<double>*, int*, int*, int*);
+    void zgetrs_(const char*, int*, int*, std::complex<double>*, int*, int*, std::complex<double>*, int*, int*);
+}
 
 void performCBFM(std::map<std::string, std::string> &const_map,
 				 std::map<int, Label> &label_map,
