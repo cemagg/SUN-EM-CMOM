08b6bdebaaaba59915dbcaa3fa1de72c3cf3b46f
Tameez <etameez@gmail.com>
2019-02-28 21:11:46 +0200

    Added now calculation of primary CBF's
    Former-commit-id: ff93a3eae51c4fabe23d760971515bbee1ab5752

--- src/cbfm/serial_cbfm/cbfm.cpp
+++ src/cbfm/serial_cbfm/cbfm.cpp
@@ -7,8 +7,12 @@ void performCBFM(std::map<std::string, std::string> &const_map,
                    std::vector<Node<double>> &nodes,
                    std::complex<double> *ilhs)
 {
+	std::ofstream file;
+	file.open("cbfm.txt");
+
 	// Define some constants
 	int num_domains = label_map.size(); // number of domains
+	int domain_size = label_map[0].edge_indices.size();
 
 	//-- Create necessary Zmn Matrices --//
 	
@@ -30,16 +34,16 @@ void performCBFM(std::map<std::string, std::string> &const_map,
                   label_map[0],
                   false);
 
-	std::cout << "---------------------------------------Z_SELF------------------------------------------" << std::endl;
-	for(int i = 0; i < label_map[0].edge_indices.size(); i++)
+	file << "---------------------------------------Z_SELF------------------------------------------" << std::endl;
+	for(int i = 0; i < domain_size; i++)
 	{
-		for(int j = 0; j < label_map[0].edge_indices.size(); j++)
+		for(int j = 0; j < domain_size; j++)
 		{
-			std::cout << v_mom_z.z_self[j + i * 4];
+			file << v_mom_z.z_self[j + i * 4];
 		}
-		std::cout << std::endl;
+		file << std::endl;
 	}
-	std::cout << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
+	file << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
 
 	// Lets now fill in v_mom_z.z_couple
 	int index;
@@ -67,22 +71,22 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 
     for(int m = 0; m < num_domains; m++){
     for(int n = 0; n < (num_domains - 1); n++){	
-    std::cout << "--------------------------------------Z_COUP"<<m<<n<<"-----------------------------------------" << std::endl;
-    for(int i = 0; i < label_map[0].edge_indices.size(); i++)
+    file << "--------------------------------------Z_COUP"<<m<<n<<"-----------------------------------------" << std::endl;
+    for(int i = 0; i < domain_size; i++)
     {
-        for(int j = 0; j < label_map[0].edge_indices.size(); j++)
+        for(int j = 0; j < domain_size; j++)
         {
-            std::cout << v_mom_z.z_couple[m][n][j + i * 4];
+            file << v_mom_z.z_couple[m][n][j + i * 4];
         }
-        std::cout << std::endl;
+        file << std::endl;
     }
-    std::cout << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
+    file << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
     }}
 	//-- Create necessary Vm vectors --//
 	CBFMVectors v_mom_v;
 
 	// Lets resize some of the cbfm vectors
-	resizeCBFMVectorsForEqualDomains(v_mom_v, num_domains, label_map[0].edge_indices.size(), false);
+	resizeCBFMVectorsForEqualDomains(v_mom_v, num_domains, domain_size, false);
 
 	// Lets now fill v_self vectors
 	for(int i = 0; i < num_domains; i++)
@@ -93,16 +97,83 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 					   v_mom_v.v_self[i],
 					   label_map[i]);
 	}
+
 	for(int m = 0; m < num_domains; m++){
-    std::cout << "--------------------------------------V_SELF"<<m<<"------------------------------------------" << std::endl;
-    for(int i = 0; i < label_map[0].edge_indices.size(); i++)
+    file << "--------------------------------------V_SELF"<<m<<"------------------------------------------" << std::endl;
+    for(int i = 0; i < domain_size; i++)
     {
-    	std::cout << v_mom_v.v_self[m][i] << std::endl;
+    	file << v_mom_v.v_self[m][i] << std::endl;
     }
-    std::cout << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
+    file << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
     }
 	//-- Calculate Primary CBF's --//
+    // First lets copy the neccessary matrices and vectors because Lapack overwrites them
+   	// Lets first copy z_self
+   	memcpy(v_mom_z.z_self_inv,
+   		   v_mom_z.z_self,
+   		   domain_size * domain_size * sizeof(std::complex<double>)); 
 
+   	file << "---------------------------------------Z_SCPY------------------------------------------" << std::endl;
+	for(int i = 0; i < domain_size; i++)
+	{
+		for(int j = 0; j < domain_size; j++)
+		{
+			file << v_mom_z.z_self_inv[j + i * 4];
+		}
+		file << std::endl;
+	}
+	file << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
+
+	// Now lets copy v_self
+	for(int i = 0; i < num_domains; i++)
+	{
+		mempcpy(v_mom_v.j_prim[i], v_mom_v.v_self[i], domain_size * sizeof(std::complex<double>));
+	}
+
+	for(int m = 0; m < num_domains; m++){
+    file << "--------------------------------------J_PCPY"<<m<<"------------------------------------------" << std::endl;
+    for(int i = 0; i < domain_size; i++)
+    {
+    	file << v_mom_v.j_prim[m][i] << std::endl;
+    }
+    file << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
+    }
+
+    // Now lets calculate primary CBF's
+
+    // First get LU Decomp of z_self
+    int z_self_lda = std::max(1, domain_size);
+    int info = 0;
+    int one = 1;
+    char tran = 'N';
+
+    zgetrf_(&domain_size, &domain_size, v_mom_z.z_self_inv, &z_self_lda, v_mom_z.z_self_piv, &info);
+
+    file << "---------------------------------------Z_SINV------------------------------------------" << std::endl;
+	for(int i = 0; i < domain_size; i++)
+	{
+		for(int j = 0; j < domain_size; j++)
+		{
+			file << v_mom_z.z_self_inv[j + i * 4];
+		}
+		file << std::endl;
+	}
+	file << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
+
+	// Now lets solve for j_prim
+	for(int i = 0; i < num_domains; i++)
+	{
+    	zgetrs_(&tran, &domain_size, &one, v_mom_z.z_self_inv, &domain_size, v_mom_z.z_self_piv, v_mom_v.j_prim[i], &domain_size, &info);
+    }
+
+	for(int m = 0; m < num_domains; m++){
+    file << "--------------------------------------J_PRIM"<<m<<"------------------------------------------" << std::endl;
+    for(int i = 0; i < domain_size; i++)
+    {
+    	file << v_mom_v.j_prim[m][i] << std::endl;
+    }
+    file << "---------------------------------------------------------------------------------------" << std::endl<<std::endl;
+    }
 	//-- Calculate Secondary CBF's --//
 
 	//-- Calculate reduced Zcbfm --//
@@ -112,4 +183,6 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 	//-- Calculate reduced Icbfm --//
 
 	//-- Solve Irwg --//
+
+	file.close();
 }
