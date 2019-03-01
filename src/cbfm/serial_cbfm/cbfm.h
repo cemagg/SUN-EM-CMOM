4518a2fceb7fd8419aa0c95225cb989ed42104bf
Tameez <etameez@gmail.com>
2019-03-01 16:36:53 +0200

    Added hook for FPGA
    Former-commit-id: c85cddaf444e98054ee5b44e6ea47275df7fe334

--- src/cbfm/serial_cbfm/cbfm.h
+++ src/cbfm/serial_cbfm/cbfm.h
@@ -36,6 +36,7 @@ void performCBFM(std::map<std::string, std::string> &const_map,
                    std::vector<Triangle> &triangles,
                    std::vector<Edge> &edges,
                    std::vector<Node<double>> &nodes,
-                   std::complex<double> *ilhs); 
+                   std::complex<double> *ilhs, 
+                   bool fpga); 
 
 #endif
