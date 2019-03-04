e81e279b3c49f1ec1b0f82e431960c0fc6891b8e
Tameez <etameez@gmail.com>
2019-03-04 10:43:34 +0200

    Commenting
    Former-commit-id: 7d6639f93b5e55f26e6a2efd63f2710f1a8ba375

--- src/data_structures/edge.h
+++ src/data_structures/edge.h
@@ -1,20 +1,32 @@
+/** 
+ * \file edge.h
+ * Defining the data structure for a MoM triangle edge
+ * 
+ * \author Tameez Ebrahim
+ * \date 04 March 2019
+ */
+
 #ifndef EDGE_H
 #define EDGE_H
 
 #include "node.h"
 
+
+/**
+ * @brief      A struct to store all information about an edge
+ */
 struct Edge
 {
-    int vertex1;
-    int vertex2;
-    Node<double> centre;
-    double length;
-    int minus_triangle_index;
-    int plus_triangle_index;
-    int minus_free_vertex;
-    int plus_free_vertex;
-    Node<double> rho_c_minus;
-    Node<double> rho_c_plus;
+    int vertex1;                // Index to node list
+    int vertex2;                // Index to node list
+    Node<double> centre;        // Centre of the edge
+    double length;              // Length of the edge
+    int minus_triangle_index;   // Index to negative triangle assosciated with the edge
+    int plus_triangle_index;    // Index to positive triangle assosciated with the edge
+    int minus_free_vertex;      // Index to negative free vertex of assosciated triangle
+    int plus_free_vertex;       // Index to positive free vertex of assosciated triangle
+    Node<double> rho_c_minus;   // Rho C Minus
+    Node<double> rho_c_plus;    // Rho C Plus
 };
 
 
