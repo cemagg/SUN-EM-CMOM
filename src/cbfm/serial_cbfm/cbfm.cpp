993d4df31baa94e8e547126a581215ce4f57c24f
Tameez <etameez@gmail.com>
2019-03-11 20:52:03 +0200

    Fixed some formatting
    Former-commit-id: 3cf78d8c9d24de6400195c7447b97452b120f062

--- src/cbfm/serial_cbfm/cbfm.cpp
+++ src/cbfm/serial_cbfm/cbfm.cpp
@@ -12,43 +12,43 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 	// file.open("cbfm.txt");
 
 	// Define some constants
-	int num_domains = label_map.size(); 							// number of domains
-	int domain_size = label_map[0].edge_indices.size();				// number of edges in domain
-   	int z_lda = std::max(1, domain_size);							// lda for LAPACK
-   	int j_lda = std::max(1, domain_size);							// lda for LAPACK
-    int info = 0;													// needed for LAPACK
-    int one = 1;													// needed for LAPACK
-    char tran = 'T';												// needed for LAPACK
-    char norm = 'N';												// needed for LAPACK
-    int index;														// indexing variable
+	int num_domains = label_map.size(); 				// number of domains
+	int domain_size = label_map[0].edge_indices.size();		// number of edges in domain
+   	int z_lda = std::max(1, domain_size);				// lda for LAPACK
+   	int j_lda = std::max(1, domain_size);				// lda for LAPACK
+	int info = 0;							// needed for LAPACK
+	int one = 1;							// needed for LAPACK
+	char tran = 'T';						// needed for LAPACK
+	char norm = 'N';						// needed for LAPACK
+	int index;							// indexing variable
 	std::complex<double> minus_one = std::complex<double>(-1.0,0.0);// needed for LAPACK
 	std::complex<double> c_zero = std::complex<double>(0.0, 0.0);	// needed for LAPACK
 	std::complex<double> c_one = std::complex<double>(1.0, 0.0);	// needed for LAPACK
-
+	
 
 	// Create struct to store all z matrices and v vectors
-	CBFMZMatrices v_mom_z; 
-    CBFMVectors v_mom_v;
+	CBFMZMatrices v_mom_z;
+	CBFMVectors v_mom_v;
 
 	// Lets resize all the struct elements to fit the problem
 	// NB: Be very aware that for v_mom_z.z_couple and v_mom_v.j_sec contain NULL 
 	//     pointers at the self terms i.e. v_mom_z.z_couple[i][j] when i==j.
 	//     This is done to allow multiple steps to go into the same loop for
 	//     speed. This is the most efficient indexing method thought of.
-    //
-    // TODO: Find a better indexing method
+	//
+	// TODO: Find a better indexing method
 	resizeCBFMZMatricesForEqualDomains(v_mom_z, num_domains, domain_size);	
 	resizeCBFMVectorsForEqualDomains(v_mom_v, num_domains, domain_size);
 
 	// Lets fill v_mom_z.z_self
 	serialFillZmn(v_mom_z.z_self,
-				  edges,
-				  triangles,
-				  nodes,
-                  const_map,
-                  label_map[0],
-                  label_map[0],
-                  false);
+		      edges,
+		      triangles,
+		      nodes,
+		      const_map,
+		      label_map[0],
+		      label_map[0],
+		      false);
 	
 	// Now copy z_self to z_self_inv because LAPACK overwites the original matrix
 	memcpy(v_mom_z.z_self_inv,
@@ -56,17 +56,17 @@ void performCBFM(std::map<std::string, std::string> &const_map,
    		   domain_size * domain_size * sizeof(std::complex<double>));
 
 	// Now get the LU-decomp of z_self_inv
-    zgetrf_(&domain_size, &domain_size, v_mom_z.z_self_inv, &z_lda, v_mom_z.z_self_piv, &info); 
+	zgetrf_(&domain_size, &domain_size, v_mom_z.z_self_inv, &z_lda, v_mom_z.z_self_piv, &info); 
 
 	// Lets now fill v_self vectors
 	for(int i = 0; i < num_domains; i++)
 	{
 		// fill v_self
 		serialFillVrhs(const_map,
-					   triangles,
-					   edges,
-					   v_mom_v.v_self[i],
-					   label_map[i]);
+			       triangles,
+			       edges,
+			       v_mom_v.v_self[i],
+			       label_map[i]);
 
 		// Copy v_self to j_prim because of LAPACK overwrites
 		memcpy(v_mom_v.j_prim[i], v_mom_v.v_self[i], domain_size * sizeof(std::complex<double>));
@@ -83,7 +83,7 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 
 	for(int i = 0; i < num_domains; i++)
 	{
-		// Caclulate primary CBF's
+	  // Caclulate primary CBF's
     	zgetrs_(&tran, &domain_size, &one, v_mom_z.z_self_inv, &domain_size,
     	        v_mom_z.z_self_piv, v_mom_v.j_prim[i], &domain_size, &info);
 
@@ -96,26 +96,26 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 			{
 				// Fill in coupling z matrices
 				serialFillZmn(v_mom_z.z_couple[j][i],
-					  		  edges,
-				  			  triangles,
-				  			  nodes,
-                  		 	  const_map,
-                  		  	  label_map[j],
-                  		  	  label_map[i],
-                  		  	  true);   
+					      edges,
+					      triangles,
+					      nodes,
+					      const_map,
+					      label_map[j],
+					      label_map[i],
+					      true);   
 				
 				// Multiply coupling z with primary CBF's and with -1
 				// Send in v_mom_v.j_sec for LAPACK to write to
 				zgemv_(&tran, &domain_size, &domain_size, &minus_one, v_mom_z.z_couple[j][i], &z_lda,
-                	v_mom_v.j_prim[i], &one, &c_zero, v_mom_v.j_sec[j][i], &one);	
+				       v_mom_v.j_prim[i], &one, &c_zero, v_mom_v.j_sec[j][i], &one);	
 
 				// Calculate secondary CBF's
 				zgetrs_(&tran, &domain_size, &one, v_mom_z.z_self_inv, &domain_size,
-    	                v_mom_z.z_self_piv, v_mom_v.j_sec[j][i], &domain_size, &info);
+					v_mom_z.z_self_piv, v_mom_v.j_sec[j][i], &domain_size, &info);
 
 				// Copy secondary CBF's to concatenated matrix -> (v_mom_v.j_cbfm)
 				std::copy(v_mom_v.j_sec[j][i], v_mom_v.j_sec[j][i] + domain_size,
-						  v_mom_v.j_cbfm[j] + (copy_index[j] * domain_size));
+					  v_mom_v.j_cbfm[j] + (copy_index[j] * domain_size));
 
 				copy_index[j]++;
 			}
@@ -153,7 +153,7 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 
 			// Calculate the reduced v vector
 			zgemv_(&tran, &domain_size, &num_domains, &c_one, v_mom_v.j_cbfm[i], &domain_size,
-				   v_mom_v.v_self[i], &one, &c_zero, v_mom_v.v_red[i], &one);
+			       v_mom_v.v_self[i], &one, &c_zero, v_mom_v.v_red[i], &one);
 
 			// Copy reduced v vector to concatenated vector
 			std::copy(v_mom_v.v_red[i], v_mom_v.v_red[i] + num_domains, v_mom_v.v_red_concat + (i * num_domains));
@@ -164,21 +164,21 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 				{
 					// Calculate Zrwg_self * Jcbfm 
 				    zgemm_(&tran, &norm, &domain_size, &num_domains, &domain_size, &c_one, v_mom_z.z_self,
-    	        		   &z_lda, v_mom_v.j_cbfm[i], &j_lda, &c_zero, c_temp, &z_lda);	
+					   &z_lda, v_mom_v.j_cbfm[i], &j_lda, &c_zero, c_temp, &z_lda);	
 
 				    // Calculate Zcbfm^-1 * [Zrwg_self * Jcbfm]
 				    zgemm_(&tran, &norm, &num_domains, &num_domains, &domain_size, &c_one, v_mom_v.j_cbfm[i],
-    	        		   &domain_size, c_temp, &domain_size, &c_zero, v_mom_z.z_red[i][j], &num_domains);
+					   &domain_size, c_temp, &domain_size, &c_zero, v_mom_z.z_red[i][j], &num_domains);
 				}
 				else
 				{
 					// Calculate Zrwg_couple * Jcbfm 
 					zgemm_(&tran, &norm, &domain_size, &num_domains, &domain_size, &c_one, v_mom_z.z_couple[i][j],
-    	        		   &z_lda, v_mom_v.j_cbfm[j], &j_lda, &c_zero, c_temp, &z_lda);	
+					       &z_lda, v_mom_v.j_cbfm[j], &j_lda, &c_zero, c_temp, &z_lda);	
 
 				    // Calculate Zcbfm^-1 * [Zrwg_couple * Jcbfm]
 					zgemm_(&tran, &norm, &num_domains, &num_domains, &domain_size, &c_one, v_mom_v.j_cbfm[i],
-    	        		   &domain_size, c_temp, &domain_size, &c_zero, v_mom_z.z_red[i][j], &num_domains);
+					       &domain_size, c_temp, &domain_size, &c_zero, v_mom_z.z_red[i][j], &num_domains);
 				}
 
 				// Now move reduced Zcbfm to concatenated matrix
@@ -189,15 +189,15 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 					if(k == 0)
 					{
 						std::copy(v_mom_z.z_red[i][j],
-								  v_mom_z.z_red[i][j] + num_domains,
-								  v_mom_z.z_red_concat + disp_index);
+							  v_mom_z.z_red[i][j] + num_domains,
+							  v_mom_z.z_red_concat + disp_index);
  						disp_index += num_domains * num_domains; 	
 					}
 					else
 					{
 						std::copy(v_mom_z.z_red[i][j] + (k * num_domains),
-								  v_mom_z.z_red[i][j] + ((k + 1) * num_domains),
-								  v_mom_z.z_red_concat + disp_index);
+							  v_mom_z.z_red[i][j] + ((k + 1) * num_domains),
+							  v_mom_z.z_red_concat + disp_index);
 						disp_index += num_domains * num_domains;
 					}
 				}	
@@ -212,28 +212,28 @@ void performCBFM(std::map<std::string, std::string> &const_map,
 
 	// Calculate Icbfm
 	zgetrf_(&num_red_rows, &num_red_rows, v_mom_z.z_red_concat, &num_red_rows, z_red_piv, &info); 
-    zgetrs_(&norm, &num_red_rows, &one, v_mom_z.z_red_concat, &num_red_rows,
-            z_red_piv, v_mom_v.v_red_concat, &num_red_rows, &info);
+	zgetrs_(&norm, &num_red_rows, &one, v_mom_z.z_red_concat, &num_red_rows,
+		z_red_piv, v_mom_v.v_red_concat, &num_red_rows, &info);
 
-    index = 0;
-    int rwg_pos = 0;
-
-    // Map CBF's scaled by Icbfm to Irwg  
+	index = 0;
+	int rwg_pos = 0;
+	
+	// Map CBF's scaled by Icbfm to Irwg  
   	for(int i = 0; i < num_domains; i++)
-  	{
-		zaxpy_(&domain_size, &v_mom_v.v_red_concat[index], v_mom_v.j_prim[i], &one, ilhs + rwg_pos, &one);
-		index++;
-
-  		for(int j = 0; j < num_domains; j++)
-  		{	
-  			if(j != i)
-  			{
-				zaxpy_(&domain_size, &v_mom_v.v_red_concat[index], v_mom_v.j_sec[i][j], &one, ilhs + rwg_pos, &one);
-  				index++;
-  			}
-  		}
-  		rwg_pos += domain_size;
-  	} 
+	  {
+	    zaxpy_(&domain_size, &v_mom_v.v_red_concat[index], v_mom_v.j_prim[i], &one, ilhs + rwg_pos, &one);
+	    index++;
+	    
+	    for(int j = 0; j < num_domains; j++)
+	      {	
+		if(j != i)
+		  {
+		    zaxpy_(&domain_size, &v_mom_v.v_red_concat[index], v_mom_v.j_sec[i][j], &one, ilhs + rwg_pos, &one);
+		    index++;
+		  }
+	      }
+	    rwg_pos += domain_size;
+	  } 
 
   	// DEBUG --------------------------------------------------------------------------------------------------------------------
 
