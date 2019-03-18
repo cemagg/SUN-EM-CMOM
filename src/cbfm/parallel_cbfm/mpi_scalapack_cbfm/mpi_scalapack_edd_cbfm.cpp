7eda7963daa4939063b18c89a2524eb084175741
Tameez <etameez@gmail.com>
2019-03-18 15:01:02 +0200

    Edited build to include parallel files
    Former-commit-id: 3288922dce3f6ef5f57ff246012d5e29b2488e20

--- src/cbfm/parallel_cbfm/mpi_scalapack_cbfm/mpi_scalapack_edd_cbfm.cpp
+++ src/cbfm/parallel_cbfm/mpi_scalapack_cbfm/mpi_scalapack_edd_cbfm.cpp
@@ -1,1 +1,11 @@
-#include "mpi_scalapack_cbfm/mpi_scalapack_edd_cbfm.h"
+#include "mpi_scalapack_cbfm/mpi_scalapack_edd_cbfm.h" 
+
+void mpiScalapackEDDCBFM(std::map<std::string, std::string> &const_map,
+                   	std::map<int, Label> &label_map,
+                 	  std::vector<Triangle> &triangles,
+                   	std::vector<Edge> &edges,
+                   	std::vector<Node<double>> &nodes,
+                   	std::complex<double> *ilhs)
+{
+	
+}
