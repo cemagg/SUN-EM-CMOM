a9809df9fb82d07a261eef7791153bd274252b23
Tameez <etameez@gmail.com>
2019-05-23 01:17:36 +0200

    Un-ignored .mom files for easy CHPC transfer
    Former-commit-id: ff2a1efd3027fc140872b92523a36dc0665baf46

---
+++ chpc_serial_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd build
+cmake -DCHPC=ON ..
+make
+
+
+
+blas/gcc/64/3.6.0
+lapack/gcc/64/3.6.0
+chpc/openbabel/2.3.1/cmake-3.7.1/gcc-4.9.0
+chpc/netcdf/4.4.4-F/gfortran/gfortran-6.1.0
+
+
+/apps/compilers/gcc/4.9.0/bin/gfortran
