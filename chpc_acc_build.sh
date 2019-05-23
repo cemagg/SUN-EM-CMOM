48636e38879f125b9d18fb9f264211e75b4ecd9e
Tameez <etameez@gmail.com>
2019-05-24 01:33:31 +0200

    Added now the needed modules for the CHPC cluster
    Former-commit-id: d8b9ad834440b8cdde67e90f0d27d014c8be21fd

--- chpc_acc_build.sh
+++ chpc_acc_build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd abuild
+module add chpc/cmake/3.11.0/gcc-6.1.0
+module add blas/gcc/64/3.6.0
+module add lapack/gcc/64/3.6.0
+module add chpc/pgi/19.4
+
+cd acc_build
 cmake -DCHPC=ON -DCACC=ON -DCMAKE_C_COMPILER="/apps/compilers/pgi/linux86-64/19.4/bin/pgcc" -DCMAKE_CXX_COMPILER="/apps/compilers/pgi/linux86-64/19.4/bin/pgc++" ..
-#cmake -DCHPC=ON -DCOMP=ON ..
 make
