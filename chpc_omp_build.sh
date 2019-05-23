1483d9c58875794a85dfbaf43ffd49235b592484
Tameez Ebrahim <tebrahim@login2.cm.cluster>
2019-05-23 03:48:43 +0200

    fixed chpc build
    Former-commit-id: 24b00cbc9aa2f9fe83d0fa83a3bff940c057fe02

--- chpc_omp_build.sh
+++ chpc_omp_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd build
-cmake -DCHPC=ON -DCACC=ON -DCMAKE_C_COMPILER="/apps/compilers/pgi/linux86-64/19.4/bin/pgcc" -DCMAKE_CXX_COMPILER="/apps/compilers/pgi/linux86-64/19.4/bin/pgc++" ..
+cmake -DCHPC=ON -DCOMP=ON -DCMAKE_C_COMPILER="/apps/compilers/pgi/linux86-64/19.4/bin/pgcc" -DCMAKE_CXX_COMPILER="/apps/compilers/pgi/linux86-64/19.4/bin/pgc++" ..
 #cmake -DCHPC=ON -DCOMP=ON ..
 make
