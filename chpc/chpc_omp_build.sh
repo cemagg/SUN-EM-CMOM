91e120972898259ad652b4dd28d3fb099dbd0a1f
Tameez Ebrahim <tebrahim@login2.cm.cluster>
2019-05-29 23:50:16 +0200

    mpi v omp results
    Former-commit-id: 04b152e45669b40d0957771d48043f363a7aee57

--- chpc/chpc_omp_build.sh
+++ chpc/chpc_omp_build.sh
@@ -4,6 +4,7 @@ module add chpc/cmake/3.11.0/gcc-6.1.0
 module add blas/gcc/64/3.6.0
 module add lapack/gcc/64/3.6.0
 module add chpc/pgi/19.4
+module add chpc/openmpi/4.0.1/pgi/19.4
 
 cd omp_build
 cmake -DCHPC=ON -DCOMP=ON -DCMAKE_C_COMPILER="/apps/compilers/pgi/linux86-64/19.4/bin/pgcc" -DCMAKE_CXX_COMPILER="/apps/compilers/pgi/linux86-64/19.4/bin/pgc++" ../..
