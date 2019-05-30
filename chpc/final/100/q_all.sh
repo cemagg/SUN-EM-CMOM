949b12f71f015552e7efaae3de0279e85f0a864a
Tameez <etameez@gmail.com>
2019-05-30 10:44:45 +0200

    Added now new jobs
    Former-commit-id: 693f2770f7adb2872c0cbf624cc6a168a702ecc9

---
+++ chpc/final/100/q_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+qsub 1.qsub
+qsub 2.qsub
+qsub 3.qsub
+qsub 4.qsub
+qsub 5.qsub
+qsub 6.qsub
+qsub 7.qsub
+qsub 8.qsub
+qsub 9.qsub
+qsub 10.qsub
+qsub 11.qsub
+qsub 12.qsub
+qsub 13.qsub
