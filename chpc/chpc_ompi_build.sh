e17e7f11e5c4b009c2049b34127e3433b7fefd77
Tameez <etameez@gmail.com>
2019-05-31 14:38:32 +0200

    new chpc test
    Former-commit-id: 2212c96b8ae349e74e6fd874aa55e8eae769be7f

--- chpc/chpc_ompi_build.sh
+++ chpc/chpc_ompi_build.sh
@@ -5,6 +5,6 @@ module add blas/gcc/64/3.6.0
 module add lapack/gcc/64/3.6.0
 module add chpc/openmpi/4.0.1/pgi/19.4
 
-cd ompi_build
+cd nompi_build
 cmake -DCHPC=ON -DCOMP=ON -DMPI=ON -DCMAKE_C_COMPILER="/apps/compilers/pgi/linux86-64/19.4/mpi/openmpi-3.1.3/bin/mpicc" -DCMAKE_CXX_COMPILER="/apps/compilers/pgi/linux86-64/19.4/mpi/openmpi-3.1.3/bin/mpic++" ../..
 make
