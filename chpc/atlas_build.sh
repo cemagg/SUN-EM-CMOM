777d19674b85437e851cff96394ccf02f52c887b
Tameez Ebrahim <tebrahim@login2.cm.cluster>
2019-06-10 12:51:23 +0200

    .
    Former-commit-id: 8a0cb2f12107c9ece87ea667ae66d0c7c4ee4988

--- chpc/atlas_build.sh
+++ chpc/atlas_build.sh
@@ -5,6 +5,6 @@ module add chpc/atlas/3.10.3/gcc-6.1.0
 module add chpc/netcdf/4.4.4-F/gfortran/gfortran-6.1.0 
 module add chpc/openmpi/4.0.0/gcc/6.1.0 
 
-cd ompi_build
+cd a
 cmake -DCATL=ON -DMPI=ON ../..
 make
