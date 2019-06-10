c2d6a091305744b387759c2715cb6e2f97bfc9b8
Tameez Ebrahim <tebrahim@login2.cm.cluster>
2019-06-10 12:03:03 +0200

    trying to get threaded lapack
    Former-commit-id: 071cbf140c8febb886da4bb1fc6816b96400b7e7

--- chpc/atlas_build.sh
+++ chpc/atlas_build.sh
@@ -3,7 +3,7 @@
 module add chpc/cmake/3.11.0/gcc-6.1.0
 module add chpc/atlas/3.10.3/gcc-6.1.0
 module add chpc/netcdf/4.4.4-F/gfortran/gfortran-6.1.0 
-moudle add chpc/openmpi/4.0.0/gcc/6.1.0 
+module add chpc/openmpi/4.0.0/gcc/6.1.0 
 
 cd ompi_build
 cmake -DCATL=ON -DMPI=ON ../..
