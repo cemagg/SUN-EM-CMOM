97d3910f7c3d3f8a76a9a1507a54c3aa90b7f9d1
Tameez <etameez@gmail.com>
2019-06-18 09:40:22 +0200

    .
    Former-commit-id: 5d3d2bfa172562bbcd0a80a76b5f39c2e641c6d1

--- chpc/atlas_build.sh
+++ chpc/atlas_build.sh
@@ -4,7 +4,8 @@ module add chpc/cmake/3.11.0/gcc-6.1.0
 module add chpc/atlas/3.10.3/gcc-6.1.0
 module add chpc/netcdf/4.4.4-F/gfortran/gfortran-6.1.0 
 module add chpc/openmpi/4.0.0/gcc/6.1.0 
+module add chpc/openblas/0.2.19/gcc-6.1.0
 
-cd a
+cd ompi_build
 cmake -DCATL=ON -DMPI=ON ../..
 make
