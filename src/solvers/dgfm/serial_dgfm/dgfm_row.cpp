4cff4e9217192f10257c62bc1ff9a6737e14ec3a
Tameez <etameez@gmail.com>
2019-06-19 13:50:38 +0200

    Added proc name print
    Former-commit-id: 6b4df9148fcc540ca63764608a877b001110333c

--- src/solvers/dgfm/serial_dgfm/dgfm_row.cpp
+++ src/solvers/dgfm/serial_dgfm/dgfm_row.cpp
@@ -74,7 +74,10 @@ void calculateDGFMRow(DGFMRow &row,
 		
 		double end = omp_get_wtime();
 
-		std::cout << "tid: " << omp_get_thread_num() <<"on: " << sched_getcpu() << " time: " << end - start << std::endl;
+	   	char processor_name[MPI_MAX_PROCESSOR_NAME];
+    	int name_len;
+    	MPI_Get_processor_name(processor_name, &name_len);	
+		std::cout << processor_name << " tid: " << omp_get_thread_num() <<" on cpu: " << sched_getcpu() << " time: " << end - start << std::endl;
 	}
 	// STOP_TIMER("Z FILL")
 	// Sum Z Matrices
--- src/solvers/dgfm/serial_dgfm/dgfm_row.h
+++ src/solvers/dgfm/serial_dgfm/dgfm_row.h
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <omp.h>
 #include <utmpx.h>
+#include <mpi.h>
 
 #include "../dgfm_helpers/dgfm_helpers.h"
 #include "../dgfm_helpers/dgfm_weights.h"
